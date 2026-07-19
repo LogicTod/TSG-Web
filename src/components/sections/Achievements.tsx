70ab2ebbe0eb797560e8de0ce5a31ce24a6ff754
LogicTod <segoro791@gmail.com>
2026-07-19 22:35:44 +0700

    -Add href button achievments

--- src/components/sections/Achievements.tsx
+++ src/components/sections/Achievements.tsx
@@ -145,7 +145,7 @@ export function Achievements({ achievements }: AchievementsProps) {
           className="mt-10 flex justify-center"
         >
           <Button
-            href="/about#achievements"
+            href="https://sites.google.com/view/thesmartgeneration/about"
             variant="secondary"
             icon={<ArrowRight className="h-4 w-4" />}
           >
