50ff71c448d79f15a77fadd71d59d9b30a29f677
LogicTod <segoro791@gmail.com>
2026-07-22 21:28:07 +0700

    -Add page gallery

--- src/components/sections/GalleryPreview.tsx
+++ src/components/sections/GalleryPreview.tsx
@@ -79,7 +79,7 @@ export function GalleryPreview({ images }: GalleryPreviewProps) {
           className="mt-10 flex justify-center"
         >
           <Button
-            href="https://sites.google.com/view/thesmartgeneration/gallery"
+            href="/gallery"
             variant="secondary"
             icon={<ArrowRight className="h-4 w-4" />}
           >
